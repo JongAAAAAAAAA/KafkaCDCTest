a9f0c018597c83a58700396a25de3ee81cbd2893
jay <wlguswhd0809@naver.com>
2023-04-25 21:31:36 +0900

    Add Repository Annotation

--- src/main/java/com/example/kafkacdctest/domain/student/dao/StudentRepository.java
+++ src/main/java/com/example/kafkacdctest/domain/student/dao/StudentRepository.java
@@ -2,7 +2,9 @@ package com.example.kafkacdctest.domain.student.dao;
 
 import com.example.kafkacdctest.domain.student.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
